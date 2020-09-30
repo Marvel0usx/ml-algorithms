cc089d4b6437a69514a516580b49a99735e9d367
marvel0usx <marvel0usx@outlook.com>
2020-09-30 10:40:57 -0600

    [dev] finished ridge regression and plots.

--- linear regression/ridge_regression_cv.py
+++ linear regression/ridge_regression_cv.py
@@ -1,15 +1,15 @@
 import numpy as np
 import pandas as pd
 
-DATA_PATH = r"./data/"
+file_path = r"../data/"
 
 data_train = {
-    "X": np.genfromtxt(DATA_PATH + "data_train_X.csv", delimiter=","),
-    "t": np.genfromtxt(DATA_PATH + "data_train_y.csv", delimiter=",")
+    "X": np.genfromtxt(file_path + "data_train_X.csv", delimiter=","),
+    "t": np.genfromtxt(file_path + "data_train_y.csv", delimiter=",")
 }
 data_test = {
-    "X": np.genfromtxt(DATA_PATH + "data_test_X.csv", delimiter=","),
-    "t": np.genfromtxt(DATA_PATH + "data_test_y.csv", delimiter=",")
+    "X": np.genfromtxt(file_path + "data_test_X.csv", delimiter=","),
+    "t": np.genfromtxt(file_path + "data_test_y.csv", delimiter=",")
 }
 
 df_train = pd.DataFrame(data_train["X"])
@@ -32,8 +32,8 @@ def split_data(data: pd.DataFrame, num_folds: int, fold: int):
     start = data.shape[0] / num_folds * (fold - 1)
     end = start + data.shape[0] / num_folds
     # mask off the validation.
-    mask = data.index.isin(np.arange(start, end))
-    return data[mask], data[~mask]
+    mask = np.arange(start, end)
+    return data.loc[data.index.isin(mask)], data.loc[~data.index.isin(mask)]    # data.loc selects rows
 
 
 def train_model(data: pd.DataFrame, lambd: float):
@@ -41,13 +41,14 @@ def train_model(data: pd.DataFrame, lambd: float):
     model with the penalty constant lambd."""
     X = data.drop("t", axis=1)
     t = data["t"]
-    w_ridge = np.linalg.inv(np.dot(X.T, X) + lambd * X.shape[0] * np.identity(X.shape[1]))
+    # Direct method to find optimal weight.
+    w_ridge = np.dot(np.dot(np.linalg.inv(np.dot(X.T, X) + lambd * np.identity(X.shape[1])), X.T), t)
     return w_ridge
 
 
 def predict(data, model):
     """Function returns predication based on data and model."""
-    return np.dot(data.drop("t", axis=1), model)
+    return np.dot(data.drop("t", axis=1), model)    # data.drop gives X
 
 
 def loss(data, model):
@@ -55,7 +56,7 @@ def loss(data, model):
     return np.power(np.linalg.norm(np.dot(data.drop("t", axis=1), model) - data["t"]), 2) / (2 * data.shape[0])
 
 
-def cross_validation(data: pd.DataFrame, num_folds: int, lambd_seq: list):
+def cross_validation(data: pd.DataFrame, num_folds: int, lambd_seq):
     """Function that computes loss for models with lambda specified in lambd_seq.
     It returns a vector of len(lambd_seq)."""
     cv_error = []
@@ -66,10 +67,47 @@ def cross_validation(data: pd.DataFrame, num_folds: int, lambd_seq: list):
         for fold in range(1, num_folds + 1):
             val_cv, train_cv = split_data(data, num_folds, fold)
             model = train_model(train_cv, lamdb)
-            cv_loss_land += loss(val_cv, model)
+            cv_loss_lmd += loss(val_cv, model)
         cv_error.append(cv_loss_lmd / num_folds)
     return cv_error
 
 
+def lambd_specific_error(lambd_seq):
+    """Function that computes error corresponding to each lambd in lambd_seq."""
+    error_list = []
+    lambd_list = []
+    for lambd in lambd_seq:
+        model = train_model(df_train, lambd)
+        train_error = loss(df_train, model)
+        test_error = loss(df_test, model)
+        error_list.append(train_error)
+        error_list.append(test_error)
+        lambd_list.extend([lambd] * 2)
+    error_df = pd.DataFrame({
+        "lambd": lambd_list,
+        "type": ["train", "test"] * len(lambd_seq),
+        "error": error_list
+    })
+    return error_df
+
+
+def plot_errors(df_errors):
+    """Function that plots errors."""
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+    g = sns.relplot(data=df_errors, x="lambd", y="error", hue="type")
+    plt.show()
+
+
 if __name__ == "__main__":
-    pass
+    lambd_seq = np.linspace(0.00005, 0.005, 50)
+    fold_5_errors = pd.DataFrame({"type": "5-fold",
+                                  "lambd": lambd_seq,
+                                  "error": cross_validation(df_train, 5, lambd_seq)})
+    fold_10_errors = pd.DataFrame({"type": "10-fold",
+                                   "lambd": lambd_seq,
+                                   "error": cross_validation(df_train, 10, lambd_seq)})
+    # Summarize all errors to dataframe.
+    df_errors = lambd_specific_error(lambd_seq).append(fold_5_errors, sort=False).append(fold_10_errors, sort=False)
+    # Plot
+    plot_errors(df_errors)
